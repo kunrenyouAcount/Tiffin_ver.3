398c6331c9ae47781d4fa6fe8f7d1cf518b5a2f5
kunrenyouAcount <1214kunrenyou@gmail.com>
2022-07-24 17:41:59 +0900

    1件取得の実装

--- src/services/masterPrefecture/interface.ts
+++ src/services/masterPrefecture/interface.ts
@@ -2,4 +2,5 @@ import { MasterPrefecture } from "../../models/masterPrefecture";
 
 export interface IMasterPrefectureService {
   findAll(): Promise<MasterPrefecture[] | Error>;
+  getById(id: number): Promise<MasterPrefecture | Error>;
 }
