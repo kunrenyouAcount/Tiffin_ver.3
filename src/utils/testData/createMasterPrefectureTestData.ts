bf511080b6975151554056db9b9d18d04570dc2c
kunrenyouAcount <1214kunrenyou@gmail.com>
2022-07-24 15:39:22 +0900

    master_prefectureの全件取得のAPIテストを実装

---
+++ src/utils/testData/createMasterPrefectureTestData.ts
@@ -0,0 +1,19 @@
+import { Connection, ResultSetHeader } from "mysql2/promise";
+import { MasterPrefecture } from "../../../src/app";
+
+export async function createMasterPrefectureTestData(connection: Connection, num: number): Promise<MasterPrefecture[]> {
+  const prefectureList: MasterPrefecture[] = [];
+
+  for (let index = 0; index < num; index++) {
+    const prefecture: MasterPrefecture = {
+      name: `test_${index}`,
+    };
+    const query = `insert into master_prefectures(name) values("${prefecture.name}")`;
+    const [result] = await connection.query<ResultSetHeader>(query);
+
+    prefecture.id = result.insertId;
+    prefectureList.push(prefecture);
+  }
+
+  return prefectureList;
+}
