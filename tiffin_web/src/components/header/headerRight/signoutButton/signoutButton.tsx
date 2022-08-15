b23c95be397028a823d8c36363c182ca4e918b57
kunrenyouAcount <1214kunrenyou@gmail.com>
2022-08-15 22:07:20 +0900

    関数名の修正

--- tiffin_web/src/components/header/headerRight/signoutButton/signoutButton.tsx
+++ tiffin_web/src/components/header/headerRight/signoutButton/signoutButton.tsx
@@ -3,13 +3,13 @@ import styles from './signoutButton.module.css'
 import { tiffin_token_key } from '@/utils/auth'
 
 export const SignoutButton: React.FC = () => {
-  const pushsignout = () => {
+  const pushSignout = () => {
     document.cookie = `${tiffin_token_key}=; max-age=0`
     router.push('/')
   }
 
   return (
-    <button className={styles.headerButton} onClick={pushsignout}>
+    <button className={styles.headerButton} onClick={pushSignout}>
       signout
     </button>
   )
