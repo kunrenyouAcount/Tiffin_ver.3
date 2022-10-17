55da35c12eda5b1d432d6b5ccbdc14e7b227bbbe
kunrenyouAcount <1214kunrenyou@gmail.com>
2022-10-18 02:12:26 +0900

    signin画面についてレイアウトの修正

--- tiffin_web/src/pages/SignIn/index.tsx
+++ tiffin_web/src/pages/SignIn/index.tsx
@@ -1,24 +1,45 @@
-import { Button } from '@mui/material'
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
+import Avatar from '@mui/material/Avatar'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Checkbox from '@mui/material/Checkbox'
+import CssBaseline from '@mui/material/CssBaseline'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import Grid from '@mui/material/Grid'
+import Link from '@mui/material/Link'
+import Paper from '@mui/material/Paper'
+import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
 import Axios from 'axios'
-import Image from 'next/image'
 import router from 'next/router'
-import { useState } from 'react'
-import { Header } from 'src/components/header'
-import { HeaderRight } from 'src/components/header/headerRight'
-import { AccountIcon } from 'src/components/header/headerRight/accountIcon'
-import { SignupButton } from 'src/components/header/headerRight/signupButton'
-import { Logo } from 'src/components/header/logo'
-import { Layout } from 'src/components/layout'
-import { AuthSigninRequest } from 'src/models/request/auth/authSigninRequest'
+import * as React from 'react'
 import { AuthResponse } from 'src/models/response/auth/authResponse'
 import { tiffin_token_key } from 'src/utils/auth'
 
-export const Signin: React.FC = () => {
-  const [user, setUser] = useState<AuthSigninRequest>()
+function Copyright(props: any) {
+  return (
+    <Typography variant='body2' color='text.secondary' align='center' {...props}>
+      {'Copyright © '}
+      <Link color='inherit' href='https://mui.com/'>
+        Tiffin
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  )
+}
 
-  const pushSignin = async () => {
-    const response = await Axios.post<AuthResponse>('auth/signin', user)
+const theme = createTheme()
+
+export default function SignIn() {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const data = new FormData(event.currentTarget)
+    const response = await Axios.post<AuthResponse>('auth/signin', {
+      email: data.get('email'),
+      password: data.get('password'),
+    })
     if (response.status == 200) {
       //cookieにjwt認証に必要な情報を保存する
       document.cookie = `${tiffin_token_key}=${response.data}`
@@ -28,53 +49,85 @@ export const Signin: React.FC = () => {
     }
   }
 
-  const changedEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const newUser = Object.assign({}, user)
-    newUser.email = e.target.value
-    setUser(newUser)
-  }
-
-  const changedPassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const newUser = Object.assign({}, user)
-    newUser.password = e.target.value
-    setUser(newUser)
-  }
-
   return (
-    <>
-      <Header>
-        <>
-          <Logo />
-          <HeaderRight>
-            <>
-              <SignupButton />
-              <AccountIcon />
-            </>
-          </HeaderRight>
-        </>
-      </Header>
-      <Layout>
-        <>
-          <Image src='/images/auth/backgroundImage.png' alt='背景画像' width='2000' height='1500' />
-          <Typography variant='h3' gutterBottom>
-            Signin
-          </Typography>
-          <div>
-            <input type='email' onChange={changedEmail} />
-            <input type='password' onChange={changedPassword} />
-            <div>
-              <Button variant='contained' onClick={() => router.back()}>
-                back
+    <ThemeProvider theme={theme}>
+      <Grid container component='main' sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: 'url(/images/auth/backgroundImage.png)',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component='h1' variant='h5'>
+              Sign in
+            </Typography>
+            <Box component='form' noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+              <TextField
+                margin='normal'
+                required
+                fullWidth
+                id='email'
+                label='Email Address'
+                name='email'
+                autoComplete='email'
+                autoFocus
+              />
+              <TextField
+                margin='normal'
+                required
+                fullWidth
+                name='password'
+                label='Password'
+                type='password'
+                id='password'
+                autoComplete='current-password'
+              />
+              <FormControlLabel
+                control={<Checkbox value='remember' color='primary' />}
+                label='Remember me'
+              />
+              <Button type='submit' fullWidth variant='contained' sx={{ mt: 3, mb: 2 }}>
+                Sign In
               </Button>
-              <Button variant='contained' onClick={pushSignin}>
-                signin
-              </Button>
-            </div>
-          </div>
-        </>
-      </Layout>
-    </>
+              <Grid container>
+                {/* <Grid item xs>
+                  <Link href='#' variant='body2'>
+                    Forgot password?
+                  </Link>
+                </Grid> */}
+                <Grid item>
+                  <Link href='/signup' variant='body2'>
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
   )
 }
-
-export default Signin
