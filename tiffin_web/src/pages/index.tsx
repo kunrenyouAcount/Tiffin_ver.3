d665d9da48033ec189a9e513d483f61107c6567d
kunrenyouAcount <1214kunrenyou@gmail.com>
2022-10-25 01:38:18 +0900

    認証系のボタンをヘッダーに移動し、ログイン判定もヘッダーで行うように改修

--- tiffin_web/src/pages/index.tsx
+++ tiffin_web/src/pages/index.tsx
@@ -1,17 +1,10 @@
-import { AppBar, Grid, Toolbar } from '@mui/material'
-import Button from '@mui/material/Button'
-import Typography from '@mui/material/Typography'
+import { Grid } from '@mui/material'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
-import { Logo } from 'src/components/logo'
-import { isLogin } from 'src/utils/auth'
+import Header from 'src/components/Header'
 
 export const Home: React.FC = () => {
   const router = useRouter()
-  const pushSignout = () => {
-    document.cookie = `tiffin_token=; max-age=0`
-    router.push('/')
-  }
 
   return (
     <div className='container'>
@@ -20,59 +13,8 @@ export const Home: React.FC = () => {
         <link rel='icon' href='/favicon.ico' />
         <meta name='viewport' content='initial-scale=1, width=device-width' />
       </Head>
-      <header>
-        <AppBar color='default' position='relative'>
-          <Toolbar>
-            <Grid container component='main'>
-              <Grid item md={11}>
-                <Logo />
-              </Grid>
-              <Grid item md={1}>
-                {isLogin() ? (
-                  <Button variant='contained' onClick={pushSignout}>
-                    signout{' '}
-                  </Button>
-                ) : (
-                  <></>
-                )}
-              </Grid>
-            </Grid>
-          </Toolbar>
-        </AppBar>
-      </header>
-      <Grid container mt={5}>
-        <Grid container alignItems='center' justifyContent='center' direction='column'>
-          <Grid item>
-            <Typography variant='h3' gutterBottom>
-              Home
-            </Typography>
-          </Grid>
-          <Grid item>
-            {isLogin() ? (
-              <></>
-            ) : (
-              <>
-                <Button
-                  variant='contained'
-                  onClick={() => {
-                    router.push('/signup')
-                  }}
-                >
-                  signup
-                </Button>
-                <Button
-                  variant='contained'
-                  onClick={() => {
-                    router.push('/signin')
-                  }}
-                >
-                  signin
-                </Button>
-              </>
-            )}
-          </Grid>
-        </Grid>
-      </Grid>
+      <Header title='Home' sections={[]}></Header>
+      <main></main>
     </div>
   )
 }
