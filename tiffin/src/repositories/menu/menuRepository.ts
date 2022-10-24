56e7c1293b9e7688759f7bf5ed4b20092f0e4d06
kunrenyouAcount <1214kunrenyou@gmail.com>
2022-10-25 00:06:53 +0900

    エリアでメニューを取得するAPIの実装

--- tiffin/src/repositories/menu/menuRepository.ts
+++ tiffin/src/repositories/menu/menuRepository.ts
@@ -9,6 +9,22 @@ export class MenuRepository implements IMenuRepository {
   constructor(connection: Connection) {
     this.connection = connection;
   }
+
+  public async getByArea(areaId: number): Promise<Menu[] | Error> {
+    try {
+      const sql =
+        //joinしたテーブルと同じカラム名が存在すると、取得したときにjoin先に上書きされてしまうため、カラム指定してselectする！
+        "select menus.id, menus.name, menus.price, menus.shop_id from menus inner join shops on menus.shop_id = shops.id where shops.master_area_id = ?";
+      const [rows] = await this.connection.execute<Menu[] & RowDataPacket[]>(sql, [areaId]);
+      if (rows.length === 0) {
+        return new NotFoundDataError(`not exists target menus`);
+      }
+      return rows as Menu[];
+    } catch (error) {
+      return new SqlError(`MenuRepository.getByArea() ERROR: ${error}`);
+    }
+  }
+
   public async getByGenre(genreId: number): Promise<Menu[] | Error> {
     try {
       const sql = "select * from menus where master_genre_id = ?";
@@ -21,6 +37,7 @@ export class MenuRepository implements IMenuRepository {
       return new SqlError(`MenuRepository.getByGenre() ERROR: ${error}`);
     }
   }
+
   public async getByCooking(cookingId: number): Promise<Menu[] | Error> {
     try {
       const sql = "select * from menus where master_cooking_id = ?";
@@ -33,10 +50,10 @@ export class MenuRepository implements IMenuRepository {
       return new SqlError(`MenuRepository.getByCooking() ERROR: ${error}`);
     }
   }
+
   public async getByStation(stationId: number): Promise<Menu[] | Error> {
     try {
       const sql =
-        //joinしたテーブルと同じカラム名が存在すると、取得したときにjoin先に上書きされてしまうため、カラム指定してselectする！
         "select menus.id, menus.name, menus.price, menus.shop_id from menus inner join shops on menus.shop_id = shops.id where shops.master_railroad_station_id = ?";
       const [rows] = await this.connection.execute<Menu[] & RowDataPacket[]>(sql, [stationId]);
       if (rows.length === 0) {
--- tiffin/src/services/menu/menuService.ts
+++ tiffin/src/services/menu/menuService.ts
@@ -8,14 +8,22 @@ export class MenuService implements IMenuService {
   constructor(menuRepository: IMenuRepository) {
     this.menuRepository = menuRepository;
   }
+
+  public async getByArea(areaId: number): Promise<Error | Menu[]> {
+    const result = await this.menuRepository.getByArea(areaId);
+    return result;
+  }
+
   public async getByGenre(genreId: number): Promise<Error | Menu[]> {
     const result = await this.menuRepository.getByGenre(genreId);
     return result;
   }
+
   public async getByCooking(cookingId: number): Promise<Error | Menu[]> {
     const result = await this.menuRepository.getByCooking(cookingId);
     return result;
   }
+
   public async getByStation(stationId: number): Promise<Error | Menu[]> {
     const result = await this.menuRepository.getByStation(stationId);
     return result;
