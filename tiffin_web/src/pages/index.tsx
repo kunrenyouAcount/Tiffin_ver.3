ba61d4dd4559ef5d5f0196de8f7cf2d89d054baf
kunrenyouAcount <1214kunrenyou@gmail.com>
2022-10-26 23:17:57 +0900

    画像リストを表示できるように実装

--- tiffin_web/src/pages/index.tsx
+++ tiffin_web/src/pages/index.tsx
@@ -1,7 +1,46 @@
-import { Grid } from '@mui/material'
+import { Grid, ImageList, ImageListItem } from '@mui/material'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import Header from 'src/components/Header'
+
+const itemData = [
+  {
+    img: '/images/sample/sample01.jpeg',
+    title: 'パエリア',
+  },
+  {
+    img: '/images/sample/sample02.png',
+    title: '照り焼きチキン',
+  },
+  {
+    img: '/images/sample/sample03.jpeg',
+    title: 'サーモンのクリーム煮',
+  },
+  {
+    img: '/images/sample/sample04.jpeg',
+    title: 'ひき肉となすのボロネーゼごはん',
+  },
+  {
+    img: '/images/sample/sample05.jpeg',
+    title: 'よだれ鶏',
+  },
+  {
+    img: '/images/sample/sample06.jpeg',
+    title: 'スタミナ丼',
+  },
+  {
+    img: '/images/sample/sample07.jpeg',
+    title: '餃子',
+  },
+  {
+    img: '/images/sample/sample08.jpeg',
+    title: '鰻丼',
+  },
+  {
+    img: '/images/sample/sample09.jpg',
+    title: 'チョコレートケーキ',
+  },
+]
 
 export const Home: React.FC = () => {
   const router = useRouter()
@@ -14,7 +53,20 @@ export const Home: React.FC = () => {
         <meta name='viewport' content='initial-scale=1, width=device-width' />
       </Head>
       <Header title='Home' sections={[]}></Header>
-      <main></main>
+      <main>
+        <ImageList sx={{ width: 1900, height: 540 }} cols={7} rowHeight={164}>
+          {itemData.map((item) => (
+            <ImageListItem key={item.img}>
+              <img
+                src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                alt={item.title}
+                loading='lazy'
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+      </main>
     </div>
   )
 }
