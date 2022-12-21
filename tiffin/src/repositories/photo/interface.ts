fe9a794df7633a9045e108c804c586461f6842cb
kunrenyouAcount <1214kunrenyou@gmail.com>
2022-12-21 23:55:15 +0900

    photoRepositoryの改修

--- tiffin/src/repositories/photo/interface.ts
+++ tiffin/src/repositories/photo/interface.ts
@@ -1,8 +1,10 @@
-import { ChoiceSearchRequest } from "../../controllers/photo/choiceSearchRequest";
+import { PhotoChoiceSearchRequest } from "../../models/api/photo/choiceSearch/request";
 import { Photo } from "../../models/photo";
 
 export interface IPhotoRepository {
   findAll(): Promise<Photo[] | Error>;
   getById(id: number): Promise<Photo | Error>;
-  choiceSearch(params: ChoiceSearchRequest): Promise<Photo[] | Error>;
+  getByMenuId(menuId: number): Promise<Photo | Error>;
+  getByMenuIds(menuIds: number[]): Promise<Photo[] | Error>;
+  choiceSearch(params: PhotoChoiceSearchRequest): Promise<Photo[] | Error>;
 }
