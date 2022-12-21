dbe8fb5fc054dc54772bd4e32fb1bd617b72643e
kunrenyouAcount <1214kunrenyou@gmail.com>
2022-12-22 00:37:11 +0900

    モーダルアイテム取得のservice層を実装

--- tiffin/src/services/menu/interface.ts
+++ tiffin/src/services/menu/interface.ts
@@ -1,4 +1,6 @@
 import { Menu } from "../../models/menu";
+import { Photo } from "../../models/photo";
+import { Shop } from "../../models/shop";
 
 export interface IMenuService {
   getByGenre(genreId: number): Promise<Menu[] | Error>;
@@ -6,4 +8,14 @@ export interface IMenuService {
   getByStation(stationId: number): Promise<Menu[] | Error>;
   getByArea(areaId: number): Promise<Menu[] | Error>;
   getByDetailedArea(detailedAreaId: number): Promise<Menu[] | Error>;
+  getModalItemByMenuId(menuId: number): Promise<
+    | {
+        menu: Menu;
+        shop: Shop;
+        photo: Photo;
+        otherMenus: Menu[];
+        otherPhotos: Photo[];
+      }
+    | Error
+  >;
 }
