b81d1e3dd8c403fceb0bab6983aaf24da51bb268
kunrenyouAcount <1214kunrenyou@gmail.com>
2023-01-10 21:54:02 +0900

    フロントで使いやすい形に再度修正

--- tiffin/src/models/api/place/searchKeyword/response.ts
+++ tiffin/src/models/api/place/searchKeyword/response.ts
@@ -1,5 +1,18 @@
 export type PlaceSearchByKeywordResponse = {
-  id: number;
-  name: string;
-  datatype: string;
+  prefectures: {
+    id: number;
+    name: string;
+  }[];
+  areas: {
+    id: number;
+    name: string;
+  }[];
+  detailedAreas: {
+    id: number;
+    name: string;
+  }[];
+  stations: {
+    id: number;
+    name: string;
+  }[];
 };
