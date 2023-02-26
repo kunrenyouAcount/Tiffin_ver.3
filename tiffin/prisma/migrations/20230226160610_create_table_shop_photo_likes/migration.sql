c173f88220f24bafff9cb2778c8b874436a3e772
kunrenyouAcount <1214kunrenyou@gmail.com>
2023-02-27 01:22:50 +0900

    prismaコマンドを使ってmigrationファイルを管理

---
+++ tiffin/prisma/migrations/20230226160610_create_table_shop_photo_likes/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE `ShopPhotoLike` (
+    `shopPhotoId` BIGINT NOT NULL,
+    `userId` BIGINT NOT NULL,
+
+    PRIMARY KEY (`shopPhotoId`, `userId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `ShopPhotoLike` ADD CONSTRAINT `ShopPhotoLike_shopPhotoId_fkey` FOREIGN KEY (`shopPhotoId`) REFERENCES `ShopPhoto`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `ShopPhotoLike` ADD CONSTRAINT `ShopPhotoLike_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
