fa2569f260618f3c1afde91fd3a49a84934cbd4b
kunrenyouAcount <1214kunrenyou@gmail.com>
2023-03-12 15:48:26 +0900

    prefectureを対応

--- tiffin/src/repositories/prefecture/interface.ts
+++
@@ -1,7 +0,0 @@
-import { Prefecture } from "../../models/prefecture";
-
-export interface IPrefectureRepository {
-  findAll(): Promise<Prefecture[] | Error>;
-  getById(id: number): Promise<Prefecture | Error>;
-  searchByKeyword(keyword: string): Promise<Prefecture[] | Error>;
-}
