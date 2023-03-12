f703f179f30610d721637a80c2071dec3e5ae106
kunrenyouAcount <1214kunrenyou@gmail.com>
2023-03-12 16:16:42 +0900

    cookingの対応

--- tiffin/src/services/cooking/cookingService.ts
+++ tiffin/src/services/cooking/cookingService.ts
@@ -1,20 +1,28 @@
-import { Cooking } from "../../models/cooking";
-import { ICookingRepository } from "../../repositories/cooking/interface";
+import { MasterCooking, PrismaClient } from "@prisma/client";
 import { ICookingService } from "./interface";
 
 export class CookingService implements ICookingService {
-  private cookingRepository: ICookingRepository;
+  private prisma: PrismaClient;
 
-  constructor(cookingRepository: ICookingRepository) {
-    this.cookingRepository = cookingRepository;
+  constructor(prisma: PrismaClient) {
+    this.prisma = prisma;
   }
-  public async getByGenre(genreId: number): Promise<Error | Cooking[]> {
-    const result = await this.cookingRepository.getByGenre(genreId);
+
+  public async getByGenre(genreId: number): Promise<MasterCooking[]> {
+    const result = await this.prisma.masterCooking.findMany({
+      where: {
+        MasterGenreMasterCooking: {
+          every: {
+            masterGenreId: genreId,
+          },
+        },
+      },
+    });
     return result;
   }
 
-  public async findAll(): Promise<Cooking[] | Error> {
-    const result = await this.cookingRepository.findAll();
+  public async findAll(): Promise<MasterCooking[]> {
+    const result = await this.prisma.masterCooking.findMany();
     return result;
   }
 }
