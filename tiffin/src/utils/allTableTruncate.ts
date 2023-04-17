91df1e515a80aeb8a139d13baa13cc5ac705f256
kunrenyouAcount <1214kunrenyou@gmail.com>
2023-04-18 00:25:52 +0900

    テーブル名の修正に伴う、truncateの修正

--- tiffin/src/utils/allTableTruncate.ts
+++ tiffin/src/utils/allTableTruncate.ts
@@ -1,13 +1,10 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 export async function truncate(prisma: PrismaClient) {
-  const allProperties = Object.keys(prisma);
-
-  const modelNames = allProperties.filter((x) => !(typeof x === "string" && (x.startsWith("$") || x.startsWith("_"))));
-  const tableNames = modelNames.map((modelName) => modelName.charAt(0).toUpperCase() + modelName.slice(1));
   // 外部キーを無効化
   await prisma.$queryRaw`SET FOREIGN_KEY_CHECKS=0`;
 
+  const tableNames = Prisma.dmmf.datamodel.models.map((model) => model.dbName) as Prisma.ModelName[];
   for (const tableName of tableNames) {
     const query = `TRUNCATE TABLE ${tableName}`;
     await prisma.$queryRawUnsafe(query);
